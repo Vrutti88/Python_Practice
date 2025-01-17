bd43ba391eb74555375d66d1a8c6b1975531f21d
Vrutti88 <vruttipatil1396@gmail.com>
2025-01-17 16:59:41 +0530

    Create ques_24.py

---
+++ ques_24.py
@@ -0,0 +1,4 @@
+# Q24) Write a simple “guess the number” game. The program randomly generates a number between 1 and 10, and the user has to guess it.
+import random
+number_to_guess = random.randint(1, 10)
+user_guess = None

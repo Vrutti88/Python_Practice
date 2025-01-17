1b1e3c37f90778667ee2b68ef6ba96e20aa28fb5
Vrutti88 <vruttipatil1396@gmail.com>
2025-01-17 16:47:42 +0530

    Create ques_15.py

---
+++ ques_15.py
@@ -0,0 +1,3 @@
+# Q15) Write a Python program to round a float to 2 decimal places without using the built-in round() function.
+n=float(input("Enter a float number: "))
+print(f"The number upto 2 decimal places is: {n:.2f}")

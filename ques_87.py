1298b1d5cd7c145790634d8993d37e4e7e001fbc
Vrutti88 <vruttipatil1396@gmail.com>
2025-01-17 17:41:15 +0530

    Create ques_87.py

---
+++ ques_87.py
@@ -0,0 +1,5 @@
+# Q87) Add a method greet to the Person class that prints "Hello, my name is <name>".
+def greet(name):
+    return f"Hello, my name is {name}"
+p=input("Enter your name: ")
+print(greet(p))

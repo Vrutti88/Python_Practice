9aaa53ceaa4471fa2c94ef715b47b2528fe37816
Vrutti88 <vruttipatil1396@gmail.com>
2025-01-17 16:44:16 +0530

    Create ques_9.py

---
+++ ques_9.py
@@ -0,0 +1,4 @@
+# Q9) Write a Python program that converts miles to kilometers. Take miles as user input.
+miles=float(input("Enter a number: "))
+km=miles*1.6
+print(f"{miles} miles = {km} kilometers")

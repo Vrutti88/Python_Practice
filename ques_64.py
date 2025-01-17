c067fa16d5309d86de96538c4f2834a98c8857f4
Vrutti88 <vruttipatil1396@gmail.com>
2025-01-17 17:27:18 +0530

    Create ques_64.py

---
+++ ques_64.py
@@ -0,0 +1,3 @@
+# Q64) Given a list with duplicates, use a set to create a list of unique elements (in any order).
+set1 = set(map(int, input("Enter duplicate elements for set1: ").split()))
+print(f"List of unique elements is: {set1}")

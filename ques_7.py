2598112853fa5c75b5019e6752a139b0a59f698b
Vrutti88 <vruttipatil1396@gmail.com>
2025-01-17 16:42:17 +0530

    Create ques_7.py

---
+++ ques_7.py
@@ -0,0 +1,6 @@
+# Q7) Write a Python program that determines if a given number is even or odd.
+num =int(input("Enter your number: "))
+if (num%2==0):
+    print("The number is even!")
+else:
+    print("The number is odd!")

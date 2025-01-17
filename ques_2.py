e3cde5bfe81842148f953f621aced9e04a039bdf
Vrutti88 <vruttipatil1396@gmail.com>
2025-01-17 14:07:06 +0530

    Create ques_2.py

---
+++ ques_2.py
@@ -0,0 +1,4 @@
+# Q2) Write a Python program that takes a user’s name and prints a greeting with that name in uppercase and lowercase.
+n=int(input("Enter your name: "))
+print(f" Hello, {n.upper()}!")
+print(f" Hello, {n.lower()}!")

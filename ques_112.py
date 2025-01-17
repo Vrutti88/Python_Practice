0128440e5aa42ca41e9630e49e4735e6da3e4831
Vrutti88 <vruttipatil1396@gmail.com>
2025-01-17 21:00:59 +0530

    Create ques_112.py

---
+++ ques_112.py
@@ -0,0 +1,4 @@
+# Q112) Use a lambda function inside map to transform a list of numbers (e.g., multiply each by 2).
+numbers = [1, 2, 3, 4, 5]
+result = list(map(lambda x: x * 2, numbers))
+print(f"Transformed list: {result}")

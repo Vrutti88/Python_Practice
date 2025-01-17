50fd978af9e3570fa36c5a3118c319c7dce76885
Vrutti88 <vruttipatil1396@gmail.com>
2025-01-17 16:42:48 +0530

    Create ques_8.py

---
+++ ques_8.py
@@ -0,0 +1,6 @@
+# Q8) Write a Python program that takes three numbers and prints the largest and the smallest numbers 
+n1=int(input("Enter first number: "))
+n2=int(input("Enter second number: "))
+n3=int(input("Enter third number: "))
+print(f"The greatest number is: {max(n1,n2,n3)}")
+print(f"The smallest number is: {min(n1,n2,n3)}")

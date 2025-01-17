4ed744aca1642ffc4695dc46e7706605423383f1
Vrutti88 <vruttipatil1396@gmail.com>
2025-01-17 16:47:16 +0530

    Create ques_14.py

---
+++ ques_14.py
@@ -0,0 +1,4 @@
+# Q14) Write a function that takes a string s and an integer n, and returns the string repeated n times.
+s=input("Enter a string: ")
+n=int(input("Enter a number: "))
+print(f"After repetition:{s * n}")

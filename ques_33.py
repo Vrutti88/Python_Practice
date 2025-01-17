4d18d5ad188a0d2199b4dd7661097d7ce04cfc7c
Vrutti88 <vruttipatil1396@gmail.com>
2025-01-17 17:04:46 +0530

    Create ques_33.py

---
+++ ques_33.py
@@ -0,0 +1,3 @@
+# Q33) Write a program to count the number of words in a user-input string (words are separated by spaces).
+n=input("Enter a string with spaces: ")
+print(f"Total number of words is = {len(n.split())}")

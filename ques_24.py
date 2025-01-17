bf87a9dad52235ed8deccb91cafb7031ae41b699
Vrutti88 <vruttipatil1396@gmail.com>
2025-01-17 17:00:58 +0530

    Update ques_24.py

--- ques_24.py
+++ ques_24.py
@@ -2,3 +2,13 @@
 import random
 number_to_guess = random.randint(1, 10)
 user_guess = None
+print("Welcome to 'Guess the Number'!")
+print("I have selected a number between 1 and 10. Try to guess it.")
+while user_guess != number_to_guess:
+    user_guess = int(input("Enter your guess: "))
+    if user_guess < number_to_guess:
+        print("Too low! Try again.")
+    elif user_guess > number_to_guess:
+        print("Too high! Try again.")
+    else:
+        print(f"Congratulations! You guessed the number {number_to_guess} correctly.")

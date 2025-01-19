6bef01eedda04b3ca3fc7baf1ca3f70481385fee
Vrutti Patil <vruttipatil1396@gmail.com>
2025-01-19 19:44:35 +0530

    Update ques_24.py

--- ques_24.py
+++ ques_24.py
@@ -1,10 +1,9 @@
 # Q24) Write a simple “guess the number” game. The program randomly generates a number between 1 and 10, and the user has to guess it.
 import random
-number_to_guess = random.randint(1, 10)
-user_guess = None
 print("Welcome to 'Guess the Number'!")
 print("I have selected a number between 1 and 10. Try to guess it.")
-while user_guess != number_to_guess:
+number_to_guess = random.randint(1, 10)
+while True:
     user_guess = int(input("Enter your guess: "))
     if user_guess < number_to_guess:
         print("Too low! Try again.")
@@ -12,3 +11,4 @@ while user_guess != number_to_guess:
         print("Too high! Try again.")
     else:
         print(f"Congratulations! You guessed the number {number_to_guess} correctly.")
+        break

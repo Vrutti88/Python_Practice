8c95e279c56471b76efa84771aeaad8301f02ded
Vrutti Patil <vruttipatil1396@gmail.com>
2025-01-19 15:01:24 +0530

    Update Assignment.py

--- Assignment.py
+++ Assignment.py
@@ -191,13 +191,10 @@ print(f"Number after reversing is: {rev}")
 import random
 number_to_guess = random.randint(1, 10)
 user_guess = None
-
 print("Welcome to 'Guess the Number'!")
 print("I have selected a number between 1 and 10. Try to guess it.")
-
 while user_guess != number_to_guess:
     user_guess = int(input("Enter your guess: "))
-
     if user_guess < number_to_guess:
         print("Too low! Try again.")
     elif user_guess > number_to_guess:

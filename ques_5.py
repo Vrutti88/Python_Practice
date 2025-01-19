e924b54e08e314ab505cba3140abcaead8b635be
Vrutti Patil <vruttipatil1396@gmail.com>
2025-01-19 13:49:48 +0530

    Update ques_5.py

--- ques_5.py
+++ ques_5.py
@@ -1,3 +1,3 @@
 # Q5) Write a Python program that asks the user to input a float number, then prints the integer part (truncate), and the fraction part separately.
 n1=float(input("Enter a float number: "))
-print(f"Integer part is: {int(n1)} and fraction part is: {abs(n1-int(n1))}")
+print(f"Integer part is: {int(n1)} and fraction part is: {n1-int(n1)}")

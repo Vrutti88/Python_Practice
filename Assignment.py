f97fe40c0838343b55a2e29153fa0c891d88a832
Vrutti Patil <vruttipatil1396@gmail.com>
2025-01-19 13:52:35 +0530

    Update Assignment.py

--- Assignment.py
+++ Assignment.py
@@ -58,7 +58,7 @@ else:
 n1=int(input("Enter first number: "))
 n2=int(input("Enter second number: "))
 n3=int(input("Enter third number: "))
-print(f"The greatest number is: {max(n1,n2,n3)}")
+print(f"The largest number is: {max(n1,n2,n3)}")
 print(f"The smallest number is: {min(n1,n2,n3)}")
 
 

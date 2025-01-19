e7ddc0bb6a7d916f488af94decd434d719a8de01
Vrutti Patil <vruttipatil1396@gmail.com>
2025-01-19 14:55:06 +0530

    Update ques_20.py

--- ques_20.py
+++ ques_20.py
@@ -2,7 +2,7 @@
 n=int(input("Enter a number: "))
 for i in range(2,int((n/2)+1)):
     if (n%i==0):
-        print("not prime")
+        print(f"{n} is not prime number")
         break
 else:
-    print("prime")
+    print(f"{n} is prime number")
